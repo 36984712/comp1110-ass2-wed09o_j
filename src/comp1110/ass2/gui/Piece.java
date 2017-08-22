9b2a4f1c207908d9d2834616964b7834ddb22f85
ruich <u6130064@anu.edu.au>
2017-08-22 11:02:19 +1000

    abstart done

---
+++ src/comp1110/ass2/gui/Piece.java
@@ -0,0 +1,15 @@
+package comp1110.ass2.gui;
+
+public enum Piece {
+
+    A(),B(),C(),D(),E(),F(),G(),H()
+    public char centroid;
+    public char rotation
+    Piece(char rotation,char centroid){
+
+    }
+    public void rotate(){}
+    public String getPlacement(){}
+
+
+}
